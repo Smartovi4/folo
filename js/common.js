6a0342888c640ff455dc4709cbf80c1286dc835a
Smartovi4 <zevzzevz@mail.ru>
2020-05-01 19:54:23 +0500

    first commit

---
+++ js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$(".top_banner").mousemove(function (e) {
+		var moveX = (e.pageX * -1 / 15);
+		var moveY = (e.pageY * -1 / 15);
+		$(this).css("background-position", moveX + "px " + moveY + "px");
+
+	});
+}); 
